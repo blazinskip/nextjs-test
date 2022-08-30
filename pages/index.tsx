c635504ca7a4cc82ff352c9cc693bdfaee6ef9fb
Patryk Błaziński <blazinski.patryk@gmail.com>
2022-08-30 21:41:43 +0200

    feat: working on mdx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,9 @@ const Home: NextPage = () => {
       </Head>
 
       <main>
-        <h1 className="text-3xl font-bold underline text-red-500">Hello World!</h1>
+        <h1 className="text-3xl font-bold underline text-red-500">
+          Hello World!
+        </h1>
       </main>
 
       <footer></footer>
